1202d003c713d2cbb6a797973c426ca31440769a
Sharon Shaji <sharonshaji127001@gmail.com>
2021-07-04 15:32:10 +0530

    Change directory name, add bgm to landing page

---
+++ splash_screen/main.js
@@ -0,0 +1,34 @@
+const progressText = document.querySelector('.loading-info')
+const banner = document.querySelector('.banner')
+
+let progress = 0
+
+const loading = () => {
+    progress++
+    if(progress === 100) {
+        clearInterval(interval)
+        const themeSong = document.querySelector('audio')
+        themeSong.play()
+        banner.style.zIndex = 2;
+    };
+
+    progressText.innerText = `${progress}% Loading...`;
+    progressText.style.opacity = mapNumber(progress, 0, 100, 1, 0)
+    banner.style.filter = `blur(${mapNumber(progress, 0, 100, 30, 0)}px)`
+}
+
+
+function mapNumber(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+const interval = setInterval(loading, 30);
+
+// toggle checkbox
+
+const checkboxs = document.querySelectorAll('.newBox')
+checkboxs.forEach(box => {
+    box.addEventListener('click', () => {
+        box.classList.toggle('active')
+    })
+})
+
