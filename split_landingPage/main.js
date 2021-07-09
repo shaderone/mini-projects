b0af93b27c69f7faf77d0f5b1c7fac90347cbc11
Sharon Shaji <sharonshaji127001@gmail.com>
2021-07-09 17:15:29 +0530

    Toggle page expansion on click
    When a click event occours, the page will expand to a 100% width

--- split_landingPage/main.js
+++ split_landingPage/main.js
@@ -6,12 +6,24 @@ bannerLeft.addEventListener('mouseenter', () => {
     container.classList.add('banner-left-active')
 })
 
+bannerLeft.addEventListener('click', () => {
+    container.classList.remove('banner-left-active')
+    container.classList.remove('banner-right-active')
+    container.classList.toggle('focused-left')
+})
+
 bannerLeft.addEventListener('mouseleave', () => {
     container.classList.remove('banner-left-active')
 })
 
 bannerRight.addEventListener('mouseenter', () => {
     container.classList.add('banner-right-active')
+})
+
+bannerRight.addEventListener('click', () => {
+    container.classList.remove('banner-left-active')
+    container.classList.remove('banner-right-active')
+    container.classList.toggle('focused-right')
 })
 
 bannerRight.addEventListener('mouseleave', () => {
