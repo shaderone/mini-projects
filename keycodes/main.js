d160a57c924b8618d837414638b80381435dadce
Sharon Shaji <sharonshaji127001@gmail.com>
2021-07-14 15:18:14 +0530

    Dynamicaly adjust font-size if necessary

--- keycodes/main.js
+++ keycodes/main.js
@@ -3,6 +3,23 @@ const key = document.querySelector('.key')
 const wrapper = document.querySelector('.wrapper')
 const toast = document.querySelector('.toast')
 
+const checkForBigKeys = (event) => {
+    const keycode = document.querySelector('h3[data-code]')
+    const actualKey = document.querySelector('h3[data-key]')
+    const keyCodeLength = event.code.length
+    const keyLength = event.key.length
+    console.log("keycodeLenght: " + keyCodeLength);
+    console.log('keylenght: ' + keyLength);
+    if(keyCodeLength>=8 && keyCodeLength <=10 || keyLength>7) {
+        key.classList.add('shrink-m')
+        keycode.classList.add('size-s')
+        actualKey.classList.add('size-s')
+    } else if(keyCodeLength >=11 && keyCodeLength <=15 || keyLength>=10) {
+        key.classList.add('shrink-l')
+        keycode.classList.add('size-m')
+    }
+}
+
 const newGradient = () => {
     var randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16),
         randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -14,7 +31,7 @@ window.addEventListener('keydown', (event) => {
     event.preventDefault()
     toast.remove()
     body.style.background = newGradient()
-    
+
     key.innerText = `${event.key === ' ' ? 'space' : event.key}`
     wrapper.innerHTML = `
         <div class="box">
@@ -27,4 +44,12 @@ window.addEventListener('keydown', (event) => {
             <h3 data-code>${event.code}</h3> <code>Event.code</code>
         </div>
     `
+    checkForBigKeys(event)
 })
+
+/*
+some things to change : 
+split event.code and add hypen
+add more space to the boxes
+scale all necessary text sizes
+*/
