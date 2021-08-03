a76412211f7579b18fdffbf722234104301d241d
Sharon Shaji <sharonshaji127001@gmail.com>
2021-08-03 19:47:42 +0530

    Add current time and date to DOM

--- analogClock/main.js
+++ analogClock/main.js
@@ -20,3 +20,26 @@ themeToggler.addEventListener('click', toggleTheme)
 function toggleTheme() {    
     document.body.classList.toggle('active-theme')
 }
+
+function DateTime() {
+
+    const timeBox = document.querySelector('.time')
+    const DateBox = document.querySelector('.fullDay')
+
+    const date = new Date()
+
+    const curHr = date.getHours()
+    let timeStatus = ''
+    if (curHr == 00 && curHr < 12) {
+        timeStatus = 'AM'
+    } else if (curHr >= 12 && curHr < 24) {
+        timeStatus = 'PM'
+    }
+    const time = date.getHours() + ":" + date.getMinutes() + ' ' + timeStatus
+    const fullDay = date.toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric' })
+
+    timeBox.innerText = time
+    DateBox.innerText = fullDay
+}
+
+DateTime()
