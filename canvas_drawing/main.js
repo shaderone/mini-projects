83d25daa4f90ad172c86820f00306acf1124c482
Sharon Shaji <sharonshaji127001@gmail.com>
2021-08-08 22:09:25 +0530

    Add functionality to interact with canvas element

---
+++ canvas_drawing/main.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d');
+
+let size = 20;
+let color = 'black';
+let x; 
+let y;
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke()
+}
+
+drawCircle(100, 100);
+drawLine(150, 150, 200, 200)
