9227be5fe43920979fa4f97ce03c57a451d72cad
Sharon Shaji <sharonshaji127001@gmail.com>
2021-08-09 17:48:21 +0530

    Add functionality to canvas tools

--- canvas_drawing/main.js
+++ canvas_drawing/main.js
@@ -1,4 +1,10 @@
 const canvas = document.getElementById('canvas')
+const tools = document.getElementById('tools')
+const minimizeBtn = document.querySelector('.minimize')
+const expandBtn = document.querySelector('.expand')
+const resetBtn = document.querySelector('.reset')
+const penSize = document.querySelector('.size')
+const colorPicker = document.getElementById('colorPicker')
 const ctx = canvas.getContext('2d');
 
 let size = 5;
@@ -47,3 +53,25 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke()
 }
 
+expandBtn.addEventListener('click', () => {
+    size += 5
+    if(size >= 50) {
+        size = 50;
+    }
+    penSize.innerText = size;
+})
+
+minimizeBtn.addEventListener('click', () => {
+    size -= 5
+    if(size <= 5) {
+        size = 5
+    }
+    penSize.innerText = size;
+})
+
+colorPicker.addEventListener('change', event => color = event.target.value)
+
+resetBtn.addEventListener('click', () => {
+    console.log('reset');
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
