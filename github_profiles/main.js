aa990502150eb3b09bef8da85eccef4485f75dc5
Sharon Shaji <sharonshaji127001@gmail.com>
2021-08-22 17:16:57 +0530

    Add empty states, display api data on page

--- github_profiles/main.js
+++ github_profiles/main.js
@@ -13,25 +13,93 @@ labelWords.forEach(label => {
 const API_URL = 'https://api.github.com/users/'
 
 const getUserData = async userName => {
+    const name = userName
     try {
         const { data } = await axios(API_URL + userName)
-        console.log(data);
+        displayUserInfoCard(data);
     } catch(err) {
-        console.log(err);
+        if(err.response.status === 404) {
+            handleError(name)
+        }
     }
 }
 
+let main = document.querySelector('#main')
+
+const displayUserInfoCard = ({login, name, bio, avatar_url, followers, following}) => {
+    const userInfoCard = `
+    <div class="card">
+        <i class="closeBtn fas fa-3x fa-times"></i>
+        <div class="profile"><img src="${avatar_url}" class="user-img" alt="${login}"></div>
+
+        <div class="user-info">
+            <ul class="stats">
+                <li>${followers}<a href="#">Followers</a></li>
+                <li>${following}<a href="#">Following</a></li>
+                <li>25<a href="#">Repositories</a></li>
+            </ul>
+            <h1 class="name">${name}</h1>
+            <p class="bio">${bio}</p>
+            <ul class="repos-info">
+                <li><a href="#">Repo 1</a></li>
+                <li><a href="#">Repo 2</a></li>
+                <li><a href="#">Repo 3</a></li>
+                <li><a href="#">Repo 4</a></li>
+            </ul>
+        </div>
+    </div>
+    `
+    main.innerHTML = userInfoCard
+}
+
+
 const form = document.querySelector('header form')
 const search = document.querySelector('#search')
+const searchIcon = document.querySelector('.search-icon img')
+const empty_state = document.querySelector('.emtpy-state')
+
+inactiveSearchHandler()
 
 form.addEventListener('submit', ev => {
     ev.preventDefault()
-    userName = search.value 
-    if(userName) {
+    handleSubmit()
+})
+
+searchIcon.addEventListener('click', () => {
+    handleSubmit()
+})
+
+
+function handleSubmit() {
+    const userName = search.value
+    if (userName) {
+        inactiveSearchHandler('onSubmit')
         getUserData(userName)
         search.value = ''
     } else {
-        console.log('no user');
+        console.log('no input to submit');
+        // please specify a username to start search
+    }
+}
+
+main.addEventListener('click', (ev) => {
+    if(ev.target.classList.contains('closeBtn')) {
+        ev.target.parentElement.remove()
+        inactiveSearchHandler()
     }
 })
 
+function handleError(userName) {
+    main.innerHTML = `<div class="emtpy-state">
+        <h1>${userName} does not exist!</h1>
+        <img src="assets/err.png" />
+    </div>`
+}
+
+function inactiveSearchHandler() {
+    main.innerHTML = `<div class="emtpy-state">
+        <h1>No active searches...</h1>
+        <img src="assets/start.png" />
+    </div>`
+
+}
