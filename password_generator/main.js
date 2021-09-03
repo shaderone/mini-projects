eae0ab64b0e4ab5ba46bf384da0a0239c4220ac1
Sharon Shaji <sharonshaji127001@gmail.com>
2021-09-03 20:02:53 +0530

    Create generator functions, remove unused styles

--- password_generator/main.js
+++ password_generator/main.js
@@ -14,3 +14,25 @@ numberField.addEventListener('input', function(){
 slider.oninput = function() {
     numberField.value = this.value
 }
+
+//Generator functions
+
+const getRandomLowerCaseLetters = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+const getRandomUpperCaseLetters = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+const getRandomNumbers = () => String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+const getRandomSymbols = () => {
+    const symbols = `!@#$%^&*()_+=-~`
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const randomChar = {
+    lower: getRandomLowerCaseLetters,
+    upper: getRandomUpperCaseLetters,
+    number: getRandomNumbers,
+    symbol: getRandomSymbols,
+}
+
+//console.log(getRandomLowerCaseLetter())
+//console.log(getRandomUpperCaseLetter())
+//console.log(getRandomNumber())
+//console.log(getRandomSymbols())
