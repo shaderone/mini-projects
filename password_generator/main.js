ccc598da0de886a1c08fcb95117ef2b5db923d1f
Sharon Shaji <sharonshaji127001@gmail.com>
2021-09-04 13:46:13 +0530

    Add functionality to generate random passwords

--- password_generator/main.js
+++ password_generator/main.js
@@ -32,6 +32,70 @@ const randomChar = {
     symbol: getRandomSymbols,
 }
 
+const resultElm = document.querySelector('#result')
+const clipboardElm = document.querySelector('.copy')
+const newResultElm = document.querySelector('.generateNew')
+//const previousElm = document.querySelector('.previous')
+//const nextElm = document.querySelector('.next')
+//const historyElm = document.querySelector('.history')
+const inputNumLengthElm = document.querySelector('#length')
+const upperCaseElm = document.querySelector('#uppercase')
+const lowerCaseElm = document.querySelector('#lowercase')
+const numbersElm = document.querySelector('#number')
+const symbolsElm = document.querySelector('#symbol')
+const generateBtn = document.querySelector('.generate-btn')
+
+const generateNewPassword = (lower, upper, number, symbol, length) => {
+    let generatedPassword = ``
+    const selectedOptions = lower + upper + number + symbol
+    const selectedOptionsArray = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+
+
+    //the code doesn't work
+    if(selectedOptions === 0) {
+        return ''
+    }
+
+    for(let index = 0; index < length; index += selectedOptions) {
+        selectedOptionsArray.forEach(selectionType => {
+            const optionName = Object.keys(selectionType)[0]
+            generatedPassword += randomChar[optionName]()
+        })
+    }
+    const finalPassword = generatedPassword.slice(0, length)
+    return finalPassword;
+}
+
+const handlePasswordRequest = () => {
+    const resultGroup = document.querySelector('.result-group')
+
+    const length = +inputNumLengthElm.value
+    const isUpperSelected = upperCaseElm.checked
+    const isLowerSelected = lowerCaseElm.checked
+    const isNumberSelected = numbersElm.checked
+    const isSymbolSelected = symbolsElm.checked
+
+    //check if atleast any single option is selected, if not throw and error
+    if (isUpperSelected === false && isLowerSelected === false && isNumberSelected === false && isSymbolSelected === false) {
+        if (document.querySelector('.error')) {
+            document.querySelector('.error').remove()
+        } else {
+            const errorBox = document.createElement('span')
+            errorBox.className = 'error'
+            errorBox.innerText = '*Please select atleast one option'
+            resultGroup.appendChild(errorBox)
+            setTimeout(() => {
+                resultGroup.removeChild(errorBox)
+            }, 1000);
+        }
+    } else if ((isUpperSelected || isLowerSelected || isNumberSelected || isSymbolSelected)) {
+        resultElm.value = generateNewPassword(isLowerSelected, isUpperSelected, isNumberSelected, isSymbolSelected, length)
+    }
+}
+
+generateBtn.addEventListener('click', handlePasswordRequest)
+newResultElm.addEventListener('click', handlePasswordRequest)
+
 //console.log(getRandomLowerCaseLetter())
 //console.log(getRandomUpperCaseLetter())
 //console.log(getRandomNumber())
