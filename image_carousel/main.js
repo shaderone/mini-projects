16dcbebe3e7db65610586c5a6dfb2dcef5f95ec4
Sharon Shaji <sharonshaji127001@gmail.com>
2021-09-13 17:17:27 +0530

    Expand and minimize individual image

--- image_carousel/main.js
+++ image_carousel/main.js
@@ -4,32 +4,30 @@ const nextBtn = document.querySelector('.arrow-right')
 const carousel_indicators = document.querySelectorAll('.carousel__indicator span')
 const carousel_image = document.querySelector('.carousel__image')
 
-class Timer {
-    constructor(fn, t) {
-        var timerObj = setInterval(fn, t)
+function Timer(fn, t) {
+    var timerObj = setInterval(fn, t);
 
-        this.stop = function () {
-            if (timerObj) {
-                clearInterval(timerObj)
-                timerObj = null
-            }
-            return this
+    this.stop = function () {
+        if (timerObj) {
+            clearInterval(timerObj);
+            timerObj = null;
         }
+        return this;
+    }
 
-        // start timer using current settings (if it's not already running)
-        this.start = function () {
-            if (!timerObj) {
-                this.stop()
-                timerObj = setInterval(fn, t)
-            }
-            return this
+    // start timer using current settings (if it's not already running)
+    this.start = function () {
+        if (!timerObj) {
+            this.stop();
+            timerObj = setInterval(fn, t);
         }
+        return this;
+    }
 
-        // start with new or original interval, stop current interval
-        this.reset = function (newT = t) {
-            t = newT
-            return this.stop().start()
-        }
+    // start with new or original interval, stop current interval
+    this.reset = function (newT = t) {
+        t = newT;
+        return this.stop().start();
     }
 }
 
@@ -66,7 +64,6 @@ const slideImage = direction => {
 
 carousel_indicators.forEach((indicator, index) => {
     indicator.addEventListener('click', (ev) => {
-        timer.reset(2000);
         activeImageIndex = index
         carousel_image.style.transform = `translateX(-${activeImageIndex}00%)`
         const getSiblings = (elm) => {
@@ -88,3 +85,22 @@ carousel_indicators.forEach((indicator, index) => {
 
 previousBtn.addEventListener('click', () => slideImage('prev'))
 nextBtn.addEventListener('click', () => slideImage('next'))
+
+const expandBoxContainer = document.querySelector('.carousel__expandBox')
+const expandBtn = document.querySelector('.carousel__container .expand')
+const minimizeBtn = expandBoxContainer.querySelector('.close')
+expandBtn.addEventListener('click', (ev) => {
+    timer.stop()
+    expandBoxContainer.classList.remove('hide')
+    const expandBox = ev.target.parentElement.previousElementSibling.firstChild.nextElementSibling
+    const img = ev.target.nextElementSibling.children[activeImageIndex];
+    const imgClone = img.cloneNode(true);
+    expandBox.appendChild(imgClone)
+    if (expandBoxContainer.firstElementChild.children.length > 2) {
+        expandBoxContainer.firstElementChild.children[1].remove()
+    }
+})
+minimizeBtn.addEventListener('click', () => {
+    expandBoxContainer.classList.add('hide')
+    timer.start()
+})
