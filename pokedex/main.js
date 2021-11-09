a066c9948e82c8da4273703ee1d1176995821964
pixiedust07 <sharonshaji127001@gmail.com>
2021-11-09 21:09:59 +0530

    Fetch pokemon data using pokeapi

--- pokedex/main.js
+++ pokedex/main.js
@@ -117,3 +117,37 @@ function handleTooltip(elm, key) {
         tooltip[tooltip_index].classList.remove('active')
     }
 }
+
+// pokeapi code
+const poke_container = document.querySelector('.poke-container')
+const pokemon_count = 150
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const fetchPokemons = async () => {
+    for (let index = 1; index < pokemon_count; index++) {
+        await getPokeman(index);
+    }
+}
+
+const getPokeman = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data);
+}
+fetchPokemons();
